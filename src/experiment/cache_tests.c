a3b894cb3841895c9289b1b13c75c2c36c21930f
Pablo Buiras <pablobuiras@gmail.com>
2019-10-31 11:25:54 +0100

    Oops

--- src/experiment/cache_tests.c
+++ src/experiment/cache_tests.c
@@ -78,9 +78,9 @@ void run_cache_experiment() {
 #ifdef RUN_2EXPS
   // run 2 cache experiments
   diff += cache_run_mult_compare(_scamv_run1, cache1, NUM_MUL_RUNS);
-  print_cache_valid(cache1);
+  //  print_cache_valid(cache1);
   diff += cache_run_mult_compare(_scamv_run2, cache2, NUM_MUL_RUNS);
-  print_cache_valid(cache2);
+  //  print_cache_valid(cache2);
   //debug_set(cache1[0], 0);
   //debug_set(cache2[0], 0);
 
