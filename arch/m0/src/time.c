ab444068ca7a0c4e28896a54d0ec1e965912b5aa
Andreas Lindner <andili@kth.se>
2019-11-20 02:14:04 +0100

    Trial of building assembly framework code for binary input

--- arch/m0/src/time.c
+++ arch/m0/src/time.c
@@ -1,6 +1,18 @@
+#include "config.h"
 #include "lib/printf.h"
+#include <stdint.h>
+
+#include "experiment/time_run.h"
+
+#ifdef RUN_TIME
 
 void run_time_experiment(void)
 {
-    printf("Paulin was here \n");
+    uint32_t t1 = _time_run(_time_run1);
+    uint32_t t2 = _time_run(_time_run2);
+
+    printf("t1 = %d\n", t1);
+    printf("t2 = %d\n", t2);
 }
+
+#endif // RUN_TIME
