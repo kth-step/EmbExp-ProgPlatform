a04d3ea94ca5e1489d72027a79aab6ec607e70ab
Paulin Gomis <gomispaulin@gmail.com>
2019-12-30 22:44:23 +0100

    Run time Experiment with setups developed

---
+++ all/inc/experiment/asm_setup1.h
@@ -0,0 +1,24 @@
+/* 
+// Experiment 1 : Beggining
+// Exepriment 1 : End  
+*/
+
+/* 
+// Experiment 2 : Beggining
+mov r0, #10
+mov r1, #5
+mul r1, r1, r0
+// Exepriment 2 : End  
+*/
+
+
+// Experiment 3 : Beggining
+mov r2, #3
+mov r3, #3
+// Exepriment 3 : End  
+
+
+/*
+// Experiments 4, 5 and 6 : Beggining
+// Experiments 4, 5 and 6 : End
+*/
--- arch/m0/src/time.c
+++ arch/m0/src/time.c
@@ -11,8 +11,8 @@ void run_time_experiment(void)
     uint32_t t1 = _time_run(_time_run1);
     uint32_t t2 = _time_run(_time_run2);
 
-    printf("t1 = %d\n", t1);
-    printf("t2 = %d\n", t2);
+    printf("T1 = %d\n", t1);
+    printf("T2 = %d\n", t2);
 }
 
 #endif // RUN_TIME
