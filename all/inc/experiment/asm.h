7c25c6f4b51f08f13e7b5a6309bf636bfbf67d69
Paulin Gomis <gomispaulin@gmail.com>
2020-01-06 13:55:05 +0100

    Test code to print the statof of COUNTFLAG added

--- all/inc/experiment/asm.h
+++ all/inc/experiment/asm.h
@@ -1,3 +1,20 @@
+b _branch0
+
+_branch0:
+    cmp r3, r2
+    beq _branch1
+    bne _branch3
+
+_branch1:
+    mov r0, #4
+    mov r1, #5
+    mul r0, r0, r1
+    sub r1, r1, #2
+    add r2, r2, #1
+    mul r0, r0, r2
+_branch3:
+    sub r0, r0, #1
+
 /* 
 // Experiment 1 : Beggining
 // Exepriment 1 : End  
@@ -11,7 +28,7 @@ mul r0, r1, r0
 // Exepriment 2 : End  
 */
 
-
+/*
 // Experiment 3 : Beggining
 b _branch0
 
@@ -29,7 +46,7 @@ _branch1:
     mul r0, r0, r2
 _branch3:
     sub r0, r0, #1
-    // Experiment 3 : End
+// Experiment 3 : End */
 
 
 /*
--- all/inc/experiment/asm_setup2.h
+++ all/inc/experiment/asm_setup2.h
@@ -1,3 +1,6 @@
+mov r2, #2
+mov r3, #1
+
 /* 
 // Experiment 1 : Beggining
 // Exepriment 1 : End  
@@ -5,27 +8,17 @@
 
 /* 
 // Experiment 2 : Beggining
-b _exp1
-
-_exp1:
-    mov r0, #10
-    add r0, r0, #1
-    mov r1, #3
-    cmp r0, r1
-    beq exp2
-exp2:
-    sub r2, r0, #4
-    mul r0, r0, r1
-mov r1, #5
+mov r2, #2
+mov r3, #1
 // Exepriment 2 : End  
 */
 
-
+/*
 // Experiment 3 : Beggining
 mov r2, #2
 mov r3, #1
 // Exepriment 3 : End  
-
+*/
 
 /*
 // Experiments 4, 5 and 6 : Beggining
