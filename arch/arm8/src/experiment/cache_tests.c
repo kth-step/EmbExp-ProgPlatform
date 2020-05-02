9ff266527f0f29f4a3270c1323c0da555499cb84
Hamed <umate@umate-vm.com>
2020-05-02 12:13:02 +0200

    debugging do_sync

--- arch/arm8/src/experiment/cache_tests.c
+++ arch/arm8/src/experiment/cache_tests.c
@@ -116,7 +116,7 @@ void run_cache_experiment() {
     printf("INCONCLUSIVE: %d\n", diff);
   }
 #elif defined RUN_1EXPS
-  _scamv_train_bp1();
+  //_scamv_train_bp1();
   diff += cache_run_mult_compare(_scamv_run1, cache, NUM_MUL_RUNS);
   print_cache_valid(cache);
   if (diff != 0)
