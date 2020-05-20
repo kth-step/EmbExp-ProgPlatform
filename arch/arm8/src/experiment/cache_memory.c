8e6907b177b6b17bff2a1987efb30c65846474d5
Hamed <umate@umate-vm.com>
2020-05-20 14:17:35 +0200

    adding function to check memory content and zeroing the cache

--- arch/arm8/src/experiment/cache_memory.c
+++ arch/arm8/src/experiment/cache_memory.c
@@ -4,6 +4,7 @@
 
 
 #include <stdint.h>
+#define CACHEABLE(x) ((void *)(((uint64_t)(x)) + 0x80000000))
 
 // memory space allocated for experiments
 extern uint64_t _experiment_memory[32 * 1024 * 8 / 8];
@@ -14,6 +15,26 @@ void _clean_experiment_memory() {
   }
 }
 
+void _zero_experiment_cache() {
+  int length = sizeof(_experiment_memory)/sizeof(uint64_t);
+  for (int i = 0; i < length; i++) {
+  	 // access a cacheable value
+  	  volatile uint64_t * yP = (uint64_t * )CACHEABLE(&_experiment_memory);
+  	  yP[i] = 0;
+  }
+}
+
+void _check_experiment_memory() {
+  int length = sizeof(_experiment_memory)/sizeof(uint64_t);
+  for (int i = 0; i < length; i++) {
+  	 // access a cacheable value
+  	  volatile uint64_t * yP = (uint64_t * )CACHEABLE(&_experiment_memory);
+  	
+  	  if (yP[i] != 0x00000000){ 	  	  
+    	  	  printf("memory content at %x is %x \n", yP + i, yP[i]);
+  	  }
+  }
+}
 
 #endif // RUN_CACHE
 
