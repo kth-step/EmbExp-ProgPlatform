2a65f4e6e8fffc017d644afb1a10481d8cf4fafc
Hamed <umate@umate-vm.com>
2021-04-12 12:10:41 +0200

    remove dup

--- arch/arm8/src/experiment/exp_cache_runner.c
+++ arch/arm8/src/experiment/exp_cache_runner.c
@@ -29,14 +29,6 @@ uint64_t expmem_byte_to_word(uint8_t v) {
           (w << 24) | (w << 16) | (w << 8 ) | (w << 0 ));
 }
 
-// memory space allocated for experiments
-extern uint64_t _experiment_memory[32 * 1024 * 8 / 8];
-void _clean_experiment_memory(uint64_t default_val) {
-  int length = sizeof(_experiment_memory)/sizeof(uint64_t);
-  for (int i = 0; i < length; i++) {
-    _experiment_memory[i] = default_val;
-  }
-}
 
 #ifdef EXP_HAS_INPUT_TRAIN
 void _clean_experiment_memory_train() {
